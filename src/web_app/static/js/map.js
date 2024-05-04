221d1f3599a626f9b28f14a29aa49de3f462867f
Max Wiesenfeld <maw5859@utexas.edu>
2024-05-03 19:58:48 -0500

    Changed default zoom level

--- src/web_app/static/js/map.js
+++ src/web_app/static/js/map.js
@@ -2,7 +2,7 @@ let map = L.map('map').setView([30.285955, -97.739320], 500)
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxNativeZoom: 19, maxZoom: 22, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map)
-map.setZoom(18)
+map.setZoom(15)
 
 let end_date_elem = document.getElementById('end_date')
 end_date_elem.addEventListener('change', (e) => {
